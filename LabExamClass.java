e0d997d00d33db6b0cbb08ab3852f8115dff37f8
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2021-05-05 19:34:36 -0700

    Create LabExamClass.java

---
+++ LabExamClass.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class LabExamClass
+{
+
+//variables
+
+
+//constructor
+public LabExamClass()
+{
+
+}
+
+//accessor methods
+
+
+//mutator methods
+
+
+//toString method
+public String toString()
+{
+   return "";
+}
+
+
+
+
+
+}
