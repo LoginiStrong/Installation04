f90aef7c41d09d58e090e4a99ec91aee5507b1dd
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2021-05-05 19:35:38 -0700

    Create RandomDieRoll.java

---
+++ RandomDieRoll.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class RandomDieRoll
+{
+   public static void main(String [] args)
+      { 
+      Random random = new Random();
+      int die = random.nextInt(20);
+      System.out.println(die);
+      
+      
+      
+      }
+}
