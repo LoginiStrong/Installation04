2d043982ee4e9d908958626bfa05dfca73c7aef0
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2021-05-05 19:35:52 -0700

    Create TicTacToeLabPartII.java

---
+++ TicTacToeLabPartII.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class TicTacToeLabPartII
+{
+   public static void main(String [] args)
+      { 
+             
+         Scanner scan = new Scanner(System.in);
+         TicTacToe tic = new TicTacToe();
+      
+         while(!tic.hasFourMovesHaveBeenplayed())
+         {
+         
+            System.out.println("Player "+tic.getCurrentPlayer()+ " please make your move:");
+         
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+      
+         
+            if(tic.play(x,y))
+            {
+            //if the move was successful
+            System.out.println("Current board is:");
+            System.out.println(tic.getBoard());
+            }
+            else
+            {
+            //if the move was invalid
+            
+            System.out.println("Player "+tic.getCurrentPlayer()+ " please make a valid move.");
+            }
+      
+      }
+      System.out.println("Four moves have been played! Game over!");
+   }
+}
