8ac3736b323a9a7ac6aaa4a2a8b54cdb2ad72dd4
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2021-05-05 19:35:12 -0700

    Create Notes3_1_21.java

---
+++ Notes3_1_21.java
@@ -0,0 +1,122 @@
+import java.util.*;
+
+public class Notes3_1_21
+{
+   public static void main(String[] args)
+   {
+   
+      /*int [] intArray = new int[3];
+      int size = 3;
+      int numberOfElementsInArray=0;
+      
+      int intFromScanner=0;
+      Scanner scan = new Scanner(System.in);
+     
+      intFromScanner = scan.nextInt();
+      do
+      {
+         intArray[numberOfElementsInArray] = intFromScanner;
+         numberOfElementsInArray++;
+
+         intFromScanner = scan.nextInt();
+      }while( intFromScanner != 0);
+*/
+
+
+/*
+         if(numberOfElementsInArray == size)
+         {
+            int [] tempArray = new int[size*2];
+            
+            for(int i=0;i<size;i++)
+               tempArray[i] = intArray[i];
+               
+            size = size*2;
+            intArray = tempArray;
+         }
+*/
+/*
+      ArrayList<String> myFirstArrayList = new ArrayList<String>();
+      
+      myFirstArrayList.add("Ducks");
+      myFirstArrayList.add("Trains");
+      myFirstArrayList.add("Planes");
+      
+      System.out.println(myFirstArrayList.get(0));
+      System.out.println(myFirstArrayList.get(1));
+      System.out.println(myFirstArrayList.get(2));
+
+
+      String [] stringArray = new String[3];
+    
+      stringArray[0] = "Ducks";  
+      stringArray[1] = "Trains";  
+      stringArray[2] = "Planes";  
+      
+      System.out.println(stringArray[0]);
+      System.out.println(stringArray[1]);
+      System.out.println(stringArray[2]);
+*/
+
+
+  
+     /* ArrayList<String>  stringList = new ArrayList<String>();
+    
+      stringList.add("Ducks");
+      stringList.add("Trains");
+      stringList.add("Chocolate");
+      stringList.add(0,"first"); //adds to 0 spot and pushes everything back
+ */
+ 
+       /*ArrayList<String> myFirstArrayList = new ArrayList<String>();
+       myFirstArrayList.add("Ducks");
+       myFirstArrayList.add("Planes");
+
+       myFirstArrayList.remove(0);
+       String element = myFirstArrayList.get(0);
+
+       myFirstArrayList.set(0, "my first element");
+       int mysize = myFirstArrayList.size();
+ */
+
+      /*ArrayList<String> myFirstArrayList = new ArrayList<String>();
+      
+      myFirstArrayList.add("Ducks");
+      myFirstArrayList.add("Trains");
+      myFirstArrayList.add("Planes");
+      
+      for(int i=0;i<myFirstArrayList.size();i++)
+      {
+         System.out.println(myFirstArrayList.get(i));
+      } 
+*/
+
+/*
+      ArrayList<String>  stringList = new ArrayList<String>();
+    
+      stringList.add("Ducks");
+      stringList.add("Trains");
+      stringList.add("Chocolate");
+      
+      for( String myString : stringList)
+      {
+         System.out.println(myString);
+      }
+ 
+*/
+
+  /*    ArrayList<Integer> myFirstArrayList = new ArrayList<Integer>();
+      myFirstArrayList.add(1);
+      myFirstArrayList.add(2);
+      myFirstArrayList.add(3);
+      
+      for(int i=0;i<myFirstArrayList.size();i++)
+      {
+         System.out.println(myFirstArrayList.get(i));
+      } 
+
+*/
+
+
+   }
+}
