84647d13c43016bbe8a88a9d477b75925aff2eb4
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2021-05-05 19:35:41 -0700

    Create RunEx.java

---
+++ RunEx.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class RunEx
+{
+   public static void main(String [] args)
+   {
+      CalcEx test = new CalcEx();
+      test.addNum();
+      test.addNum();
+      test.subNum();
+      System.out.println(test);
+   
+   
+   }
+}
