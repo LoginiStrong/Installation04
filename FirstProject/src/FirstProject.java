eb399ac3945bfbd25c08aa4c3b73e775884c8c1c
LoginiStrong <sandmanlms@charter.net>
2022-09-01 23:34:25 -0700

    Starting first assignment

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -18,11 +18,18 @@ public class FirstProject {
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
 
+        HttpRequest newRequest = HttpRequest.newBuilder().uri(URI.create("https://www.surlatable.com/shrimp-boil/REC-390893.html?cgid=recipes")) //starting URL…
+                .GET() // GET is default
+                .build();
+
+        HttpResponse<String> newResponse = client.send(newRequest,
+                HttpResponse.BodyHandlers.ofString());
+
 
 //output to a file so its easy to mess with (you won’t be for your finished program)
         FileOutputStream fs = new FileOutputStream("output.txt");
         PrintWriter pw = new PrintWriter(fs);
-        pw.println(response.body()); //response.body() is the html source code in a string format. It outputs to a file so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
+        pw.println(newResponse.body()); //response.body() is the html source code in a string format. It outputs to a file so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
         pw.close();
     }
 }
---
+++ Website/src/Website.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.*;
+
+    public class Website {
+
+       public static void main(String[] args) throws IOException, InterruptedException {
+
+            HttpClient client = HttpClient.newHttpClient();
+            /*HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://www.surlatable.com/recipes/?srule=best-matches&start=0&sz=24")) //starting URL…
+                    .GET() // GET is default
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());*/
+
+            HttpRequest newRequest = HttpRequest.newBuilder().uri(URI.create("https://www.surlatable.com/shrimp-boil/REC-390893.html?cgid=recipes")) //starting URL…
+                    .GET() // GET is default
+                    .build();
+
+            HttpResponse<String> newResponse = client.send(newRequest,
+                    HttpResponse.BodyHandlers.ofString());
+
+
+//output to a file, so it's easy to mess with (you won’t be for your finished program)
+            FileOutputStream fs = new FileOutputStream("output.csv");
+            PrintWriter pw = new PrintWriter(fs);
+            pw.println(newResponse.body()); //response.body() is the html source code in a string format. It outputs to a file so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
+            pw.close();
+        }
+    }
+
